70c540656eea9405e37da9cae980ed4339b11224
Mai Mouri <sfyitan@gmail.com>
2022-05-31 19:24:41 +0900

    Initial Commit

---
+++ Dishee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dishee
+//
+//  Created by Mai Mouri on 2022/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
